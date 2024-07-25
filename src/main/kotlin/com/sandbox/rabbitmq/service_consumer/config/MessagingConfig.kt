3b94edfa7ff63cc57ccecd3d87581e86949aac13
aassalax <aassali.dev@gmail.com>
2024-07-25 16:55:06 +0200

    consume POJO

--- src/main/kotlin/com/sandbox/rabbitmq/service_consumer/config/MessagingConfig.kt
+++ src/main/kotlin/com/sandbox/rabbitmq/service_consumer/config/MessagingConfig.kt
@@ -1,6 +1,12 @@
 package com.sandbox.rabbitmq.service_consumer.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.amqp.core.*
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -22,5 +28,20 @@ class MessagingConfig {
         autoDeleteQueue: Queue?
     ): Binding {
         return BindingBuilder.bind(autoDeleteQueue).to(fanout)
+    }
+
+    @Bean
+    fun jackson2JsonMessageConverter(): Jackson2JsonMessageConverter {
+        val objectMapper = ObjectMapper()
+            .registerModule(KotlinModule.Builder().build())
+            .registerModule(JavaTimeModule())
+        return Jackson2JsonMessageConverter(objectMapper)
+    }
+
+    @Bean
+    fun rabbitTemplate(connectionFactory: ConnectionFactory, converter: Jackson2JsonMessageConverter): RabbitTemplate {
+        val rabbitTemplate = RabbitTemplate(connectionFactory)
+        rabbitTemplate.messageConverter = converter
+        return rabbitTemplate
     }
 }
--- src/main/kotlin/com/sandbox/rabbitmq/service_consumer/infrastructure/messaging/MessageConsumer.kt
+++ src/main/kotlin/com/sandbox/rabbitmq/service_consumer/infrastructure/messaging/MessageConsumer.kt
@@ -4,21 +4,29 @@ import com.sandbox.rabbitmq.service_consumer.domain.applicationservice.port.inpu
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Component
 import org.springframework.util.StopWatch
+import java.time.LocalDateTime
 
 @Component
-class MessageConsumer() : Consumer<String> {
+class MessageConsumer() : Consumer<MessageEvent> {
 
     @RabbitListener(queues = ["#{autoDeleteQueue.name}"])
-    override fun consume(event: String) {
+    override fun consume(event: MessageEvent) {
         val watch = StopWatch()
         watch.start()
-        println("Handling message: $event")
+        println("Handling event: $event")
         handle(event)
         watch.stop()
         println("[x] Done in ${watch.totalTimeSeconds} s")
     }
 
-    private fun handle(message: String) {
-        println("processing event: $message")
+    private fun handle(event: MessageEvent) {
+        println("processing event: $event")
+        println("The message of event is : ${event.message.value}")
     }
 }
+data class MessageEvent(
+    val message: Message,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+)
+data class Message(val value: String)
+
